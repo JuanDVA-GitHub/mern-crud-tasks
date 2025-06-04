90b8c76142d19e7b60c2e37cfde837bc12d018cc
Juan Villanueva <jd.234-jk@hotmail.com>
2025-06-03 23:14:49 -0500

    cluster creado frontend

--- src/database.js
+++ src/database.js
@@ -1,14 +1,19 @@
 import mongoose from "mongoose";
 import { MONGODB_URI } from "./config.js";
 
-try {
-  mongoose.set("strictQuery", false);
-  const conn = await mongoose.connect(MONGODB_URI);
-  console.log(`MongoDB Connected: ${conn.connection.host}`);
-} catch (error) {
-  console.error({ error });
+mongoose.set("strictQuery", false);
+
+async function connectDB() {
+  try {
+    const conn = await mongoose.connect(MONGODB_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error({ error });
+  }
 }
 
+connectDB();
+
 mongoose.connection.on("connected", () => {
-  console.log("Database is connected to", connection.db.databaseName);
+  console.log("Database is connected to", mongoose.connection.db.databaseName);
 });
